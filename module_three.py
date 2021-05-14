4e063b1e527718819373afc86f4451297426c598
AaronBlau <aaron.blaufox@exosonic.com>
2021-05-13 23:52:36 -0400

    fixed typo. added some lines

--- module_three.py
+++ module_three.py
@@ -1,10 +1,14 @@
 def report():
     ORIGINAL_NUM = 0
     
+    
+    
     num = 0
+    
     originality = {True  : 'This is the original number.',
                    False : 'This is NOT the original number. Edits have been made.'}
     b_is_og = (num == ORIGINAL_NUM)    
-    print('module_one: Why hello there!')
+    print('module_three: Why hello there!')
     print('    num is {} \n    {}'.format(num, originality[b_is_og]))
+    print("    Also, I used to have a typo. Now I don't.")
     return b_is_og
